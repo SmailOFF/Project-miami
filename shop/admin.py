e368d376e06c8d4f5ae96acf81073ce27edfe6c4
Gulomali <vladislavxan734@gmail.com>
2022-04-26 16:13:44 +0300

    New project

--- shop/admin.py
+++ shop/admin.py
@@ -6,9 +6,9 @@ class SlugAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 
-admin.site.register(Product, SlugAdmin)
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(Slide)
-admin.site.register(Order)
-admin.site.register(OrderProduct)
+# admin.site.register(Product, SlugAdmin)
+# admin.site.register(Category)
+# admin.site.register(Brand)
+# admin.site.register(Slide)
+# admin.site.register(Order)
+# admin.site.register(OrderProduct)
